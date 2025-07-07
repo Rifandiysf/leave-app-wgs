eacaf7d2607377446b6bdfaedc0600311a642fbb
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-07 20:29:15 +0700

    feat: History Users

---
+++ client/app/history/page.tsx
@@ -0,0 +1,177 @@
+'use client'
+
+import { ReactNode, useState } from "react"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { SearchButton } from "../components/search/page"
+import { SelectDemo } from "../components/select/page"
+import { SelectItem, SelectLabel } from "../components/ui/select"
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious
+} from "../components/ui/pagination"
+
+type LeaveHistoryType = {
+    status: ReactNode
+    type: string
+    startLeave: string
+    endLeave: string
+    leaveUsage: string
+    reason: string
+    note: ReactNode
+}
+
+const HistoryPage = () => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const ITEMS_PER_PAGE = 8
+
+    const HistoryLeave: LeaveHistoryType[] = [
+        {
+            status: <span className="bg-[#FFEAB2] p-2 px-3 rounded-full text-xs">WAITING</span>,
+            type: "Personal",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        {
+            status: <span className="bg-[#FF7C7C] p-2 px-3 rounded-full text-xs">REJECT</span>,
+            type: "Mandatory",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        {
+            status: <span className="bg-[#A0F3AC] p-2 px-3 rounded-full text-xs">APPROVE</span>,
+            type: "Personal",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        {
+            status: <span className="bg-[#FFEAB2] p-2 px-3 rounded-full text-xs">WAITING</span>,
+            type: "Personal",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        {
+            status: <span className="bg-[#FFEAB2] p-2 px-3 rounded-full text-xs">WAITING</span>,
+            type: "Spesial",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        {
+            status: <span className="bg-[#FF7C7C] p-2 px-3 rounded-full text-xs">REJECT</span>,
+            type: "Mandatory",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        {
+            status: <span className="bg-[#A0F3AC] p-2 px-3 rounded-full text-xs">APPROVE</span>,
+            type: "Personal",
+            startLeave: "15 January 2025",
+            endLeave: "16 January 2025",
+            leaveUsage: "2 day(s)",
+            reason: "Family",
+            note: <i className="bi bi-exclamation-circle-fill text-2xl cursor-pointer"></i>
+        },
+        
+    ]
+
+    const totalPages = Math.ceil(HistoryLeave.length / ITEMS_PER_PAGE)
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE
+    const currentData = HistoryLeave.slice(startIndex, startIndex + ITEMS_PER_PAGE)
+
+    return (
+        <>
+            <section className="flex justify-end items-center p-5 border-b-[1.5px] border-[#0000001f]">
+                <SearchButton />
+                <SelectDemo placeholder="Status">
+                    <SelectLabel>Status</SelectLabel>
+                    <SelectItem value="approve">Approve</SelectItem>
+                    <SelectItem value="waiting">Waiting</SelectItem>
+                    <SelectItem value="reject">Reject</SelectItem>
+                </SelectDemo>
+            </section>
+
+            <section className="relative p-3 min-h-[calc(100dvh-137px)]">
+                <table className="w-full table-auto rounded-t-2xl">
+                    <thead className="border-b-[1.5px] border-[#0000001f] bg-[#f0f4f9] rounded-2xl shadow-2xl">
+                        <tr>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">Status</th>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">Type</th>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">Start Leave</th>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">End Leave</th>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">Leave Usage</th>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">Reason</th>
+                            <th className="p-3 text-[18px] font-semibold tracking-wide">Note</th>
+                        </tr>
+                    </thead>
+                    <tbody className="cursor-pointer">
+                        {currentData.map((data, idx) => (
+                            <tr key={idx} className="odd:bg-[#e8efff] even:bg-[#f8faff] hover:bg-[#e3e7f0] transition-colors duration-300">
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.status}</th>
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.type}</th>
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.startLeave}</th>
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.endLeave}</th>
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.leaveUsage}</th>
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.reason}</th>
+                                <th className="p-3 text-[14px] font-medium border-b-[1.5px] border-[#0000001f]">{data.note}</th>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <div className="flex justify-center items-center bg-white py-5">
+                    <Pagination>
+                        <PaginationContent>
+                            <PaginationItem>
+                                <PaginationPrevious
+                                    onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                                    className={currentPage === 1 ? "pointer-events-none opacity-50" : ""}
+                                />
+                            </PaginationItem>
+
+                            {Array.from({ length: totalPages }, (_, i) => (
+                                <PaginationItem key={i}>
+                                    <PaginationLink
+                                        isActive={currentPage === i + 1}
+                                        onClick={() => setCurrentPage(i + 1)}
+                                    >
+                                        {i + 1}
+                                    </PaginationLink>
+                                </PaginationItem>
+                            ))}
+
+                            <PaginationItem>
+                                <PaginationNext
+                                    onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+                                    className={currentPage === totalPages ? "pointer-events-none opacity-50" : ""}
+                                />
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default HistoryPage
