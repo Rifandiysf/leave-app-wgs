5399b0046f2683104e0ded0a2685d1223968a284
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-08 16:52:17 +0700

    feat: Props for Placeholder

--- client/app/components/search/page.tsx
+++ client/app/components/search/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/app/components/ui/button"
 import { Input } from "@/app/components/ui/input"
 
-export function SearchButton() {
+type SearchButtonPropsType = {
+    placeholder?: string
+}
+
+export function SearchButton({ placeholder }: SearchButtonPropsType) {
     return (
         <div className="flex w-full max-w-[15rem] items-center gap-2">
-            <Input type="search" placeholder="Search History" className="w-44"/>
+            <Input type="search" placeholder={placeholder} className="w-44"/>
             <Button type="submit" variant="default">
                 <i className="bi bi-search"></i>
             </Button>
