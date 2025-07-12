6b24be1d2b83ef6f7edcbf04fd4bb2e15e60c301
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-12 21:21:56 +0700

    add: import bootstrap icon

--- client/app/auth/login/page.tsx
+++ client/app/auth/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { Button } from "../../components/ui/button"
