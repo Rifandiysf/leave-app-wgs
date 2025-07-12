beb781a9c48ff09b9034b91617d48c902ce025ed
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-12 21:24:16 +0700

    feat: create role props

--- client/app/components/layout/header.tsx
+++ client/app/components/layout/header.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-export default function Header() {
+import Link from "next/link"
 
+type HeaderProps = {
+    role?: "admin" | "user"
+}
+
+export default function Header({ role = "user" }: HeaderProps) {
     return (
         <header className="flex items-center justify-between mb-8 sm:mb-4">
-
             <div className="flex-1"></div>
-
             <div className="sm:hidden flex items-center space-x-2">
                 <button className="p-1.5 hover:bg-blue-50 rounded-lg transition-colors cursor-pointer">
                     <i className="bi bi-gear-fill text-lg text-gray-600 hover:text-blue-900" />
@@ -17,15 +20,36 @@ export default function Header() {
             </div>
 
             <div className="hidden sm:flex items-center space-x-6">
-                <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
-                    <i className="bi bi-gear-fill text-xl" />
-                    <span className="text-sm font-medium">Settings</span>
-                </div>
+                {role === "admin" ? (
+                    <>
+                        <Link href={'/admin/dashboard'} className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
+                            <i className="bi bi-person-workspace text-xl w-6 text-center" />
+                            <span className="text-sm font-medium">Admin sitee</span>
+                        </Link>
 
-                <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
-                    <i className="bi bi-box-arrow-right text-xl" />
-                    <span className="text-sm font-medium">Logout</span>
-                </div>
+                        <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
+                            <i className="bi bi-gear-fill text-xl" />
+                            <span className="text-sm font-medium">Settings</span>
+                        </div>
+
+                        <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
+                            <i className="bi bi-box-arrow-right text-xl" />
+                            <span className="text-sm font-medium">Logout</span>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
+                            <i className="bi bi-gear-fill text-xl" />
+                            <span className="text-sm font-medium">Settings</span>
+                        </div>
+
+                        <div className="flex items-center space-x-2 cursor-pointer hover:text-blue-900 transition-colors">
+                            <i className="bi bi-box-arrow-right text-xl" />
+                            <span className="text-sm font-medium">Logout</span>
+                        </div>
+                    </>
+                )}
             </div>
         </header>
     )
--- client/app/components/layout/sidebar.tsx
+++ client/app/components/layout/sidebar.tsx
@@ -5,7 +5,11 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-export default function Sidebar() {
+type sideBarProps = {
+    role?: "admin" | "user"
+}
+
+export default function Sidebar({ role = "user", }: sideBarProps) {
     const pathname = usePathname()
 
     const isActive = (path: string) =>
@@ -16,76 +20,152 @@ export default function Sidebar() {
 
     return (
         <>
-            {/* Sidebar for desktop only */}
-            <aside className="w-64 flex-col hidden lg:flex bg-white h-full fixed lg:relative z-50">
-                <div className="bg-white p-7 pb-3">
-                    <div className="flex items-center justify-between">
-                        <div>
-                            <Image
-                                src="/images/logo-wgs.svg"
-                                alt="Logo WGS"
-                                width={120}
-                                height={40}
-                                priority
-                            />
-                            <h2 className="text-2xl font-medium text-black mt-2">Welcome, User</h2>
+            {role === "admin" ? (
+                <>
+                    <aside className="w-64 flex-col hidden lg:flex bg-white h-full fixed lg:relative z-50">
+                        <div className="bg-white p-7 pb-3">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <Image
+                                        src="/images/logo-wgs.svg"
+                                        alt="Logo WGS"
+                                        width={120}
+                                        height={40}
+                                        priority
+                                    />
+                                    <h2 className="text-2xl font-medium text-black mt-2">Welcome, Admin</h2>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <nav className="bg-blue-100 p-4 flex-1 pt-5 relative rounded-se-4xl">
-                    <div className="space-y-6 text-blue-900 font-semibold mt-8">
-                        <Link
-                            href="/"
-                            className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/')}`}
-                        >
-                            <i className="bi bi-person-workspace text-xl w-6 text-center" />
-                            <span className="ml-3">Dashboard</span>
-                        </Link>
-                        <Link
-                            href="/history"
-                            className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/history')}`}
-                        >
-                            <i className="bi bi-clock-history text-xl w-6 text-center" />
-                            <span className="ml-3">History</span>
-                        </Link>
-                        <Link
-                            href="/special"
-                            className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/special')}`}
-                        >
-                            <i className="bi bi-asterisk text-xl w-6 text-center" />
-                            <span className="ml-3">Special</span>
-                        </Link>
-                        <Link
-                            href="/mandatory"
-                            className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/mandatory')}`}
-                        >
-                            <i className="bi bi-archive-fill text-xl w-6 text-center" />
-                            <span className="ml-3">Mandatory</span>
-                        </Link>
-                    </div>
-                </nav>
-            </aside>
+                        <nav className="bg-blue-100 p-4 flex-1 pt-5 relative rounded-se-4xl">
+                            <div className=" font-semibold mt-8">
+                                <Link
+                                    href="/admin/dashboard"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/admin/dashboard')}`}
+                                >
+                                    <i className="bi bi-person-workspace text-xl w-6 text-center" />
+                                    <span className="ml-3">Dashboard</span>
+                                </Link>
+                                <div className="mt-4 h-px bg-gray-500 mb-2  " />
+                                <Link
+                                    href="/admin/list-leave"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/admin/list-leave')}`}
+                                >
+                                    <i className="bi bi-clock-history text-xl w-6 text-center" />
+                                    <span className="ml-3">List Of Leave</span>
+                                </Link>
+                                <Link
+                                    href="/admin/mandatory"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/admin/mandatory')}`}
+                                >
+                                    <i className="bi bi-asterisk text-xl w-6 text-center" />
+                                    <span className="ml-3">Mandatory</span>
+                                </Link>
+                                <Link
+                                    href="/admin/special-leave"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/admin/special-leave')}`}
+                                >
+                                    <i className="bi bi-archive-fill text-xl w-6 text-center" />
+                                    <span className="ml-3">Special Leave</span>
+                                </Link>
+                            </div>
+                        </nav>
+                    </aside>
 
-            {/* Bottom navigation for mobile only */}
-            <nav className="fixed bottom-0 left-0 w-full bg-white border-t z-50 flex justify-around items-center py-2 lg:hidden">
-                <Link href="/" className="flex flex-col items-center text-xs">
-                    <i className={`bi bi-person-workspace text-xl ${isBottomActive('/')}`} />
-                    <span>Dashboard</span>
-                </Link>
-                <Link href="/history" className="flex flex-col items-center text-xs">
-                    <i className={`bi bi-clock-history text-xl ${isBottomActive('/history')}`} />
-                    <span>History</span>
-                </Link>
-                <Link href="/special" className="flex flex-col items-center text-xs">
-                    <i className={`bi bi-asterisk text-xl ${isBottomActive('/special')}`} />
-                    <span>Special</span>
-                </Link>
-                <Link href="/mandatory" className="flex flex-col items-center text-xs">
-                    <i className={`bi bi-archive-fill text-xl ${isBottomActive('/mandatory')}`} />
-                    <span>Mandatory</span>
-                </Link>
-            </nav>
+                    {/* Bottom navigation for mobile only */}
+                    <nav className="fixed bottom-0 left-0 w-full bg-white border-t z-50 flex justify-around items-center py-2 lg:hidden">
+                        <Link href="/admin/dashboard" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-person-workspace text-xl ${isBottomActive('/admin/dashboard')}`} />
+                            <span>Dashboard</span>
+                        </Link>
+                        <Link href="/admin/list-leave" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-clock-history text-xl ${isBottomActive('/admin/list-leave')}`} />
+                            <span>List Of Leave</span>
+                        </Link>
+                        <Link href="/admin/mandatory" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-asterisk text-xl ${isBottomActive('/admin/mandatory')}`} />
+                            <span>Mandatory</span>
+                        </Link>
+                        <Link href="/admin/special-leave" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-archive-fill text-xl ${isBottomActive('/admin/special-leave')}`} />
+                            <span>Special Leave</span>
+                        </Link>
+                    </nav>
+                </>
+            ) : (
+                <>
+                    <aside className="w-64 flex-col hidden lg:flex bg-white h-full fixed lg:relative z-50">
+                        <div className="bg-white p-7 pb-3">
+                            <div className="flex items-center justify-between">
+                                <div>
+                                    <Image
+                                        src="/images/logo-wgs.svg"
+                                        alt="Logo WGS"
+                                        width={120}
+                                        height={40}
+                                        priority
+                                    />
+                                    <h2 className="text-2xl font-medium text-black mt-2">Welcome, User</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <nav className="bg-blue-100 p-4 flex-1 pt-5 relative rounded-se-4xl">
+                            <div className="space-y-6 text-blue-900 font-semibold mt-8">
+                                <Link
+                                    href="/"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/')}`}
+                                >
+                                    <i className="bi bi-person-workspace text-xl w-6 text-center" />
+                                    <span className="ml-3">Dashboard</span>
+                                </Link>
+                                <Link
+                                    href="/history"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/history')}`}
+                                >
+                                    <i className="bi bi-clock-history text-xl w-6 text-center" />
+                                    <span className="ml-3">History</span>
+                                </Link>
+                                <Link
+                                    href="/special"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/special')}`}
+                                >
+                                    <i className="bi bi-asterisk text-xl w-6 text-center" />
+                                    <span className="ml-3">Special</span>
+                                </Link>
+                                <Link
+                                    href="/mandatory"
+                                    className={`flex items-center py-3 px-6 rounded-full transition-colors ${isActive('/mandatory')}`}
+                                >
+                                    <i className="bi bi-archive-fill text-xl w-6 text-center" />
+                                    <span className="ml-3">Mandatory</span>
+                                </Link>
+                            </div>
+                        </nav>
+                    </aside>
+
+                    {/* Bottom navigation for mobile only */}
+                    <nav className="fixed bottom-0 left-0 w-full bg-white border-t z-50 flex justify-around items-center py-2 lg:hidden">
+                        <Link href="/" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-person-workspace text-xl ${isBottomActive('/')}`} />
+                            <span>Dashboard</span>
+                        </Link>
+                        <Link href="/history" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-clock-history text-xl ${isBottomActive('/history')}`} />
+                            <span>History</span>
+                        </Link>
+                        <Link href="/special" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-asterisk text-xl ${isBottomActive('/special')}`} />
+                            <span>Special</span>
+                        </Link>
+                        <Link href="/mandatory" className="flex flex-col items-center text-xs">
+                            <i className={`bi bi-archive-fill text-xl ${isBottomActive('/mandatory')}`} />
+                            <span>Mandatory</span>
+                        </Link>
+                    </nav>
+                </>
+            )}
         </>
     )
 }
