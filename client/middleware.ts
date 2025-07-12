d11696af7e1c7eaff0f23880c830253ac541e9e8
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-12 21:25:42 +0700

    feat: Protected route

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,12 +1,30 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+
+export default function middleware(request: NextRequest) {
+    const session = request.cookies.get('connect.sid')?.value
+    const { pathname } = request.nextUrl
+
+    const isLoginPage = pathname === '/auth/login'
+    const isPublicPath = isLoginPage
+
+    if (!session && !isPublicPath) {
+        return NextResponse.redirect(new URL('/auth/login'))
+    }
+
+    if (session && isPublicPath) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    // if (pathname.startsWith('/admin') && session !== 'admin') {
+    //     return NextResponse.redirect(new URL('/', request.url))
+    // }
+
+    return NextResponse.next()
+}
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|logo.png|images|fonts).*)',
     ],
 };
