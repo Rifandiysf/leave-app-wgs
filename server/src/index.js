dc781956c30a3c91ddf538dcfc8a39fdf5dba341
Andhika Dwitama <andhikaDWA@gmail.com>
2025-07-14 14:54:03 +0700

    Add request time out middleware

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,15 @@ import express, { urlencoded } from 'express';
 import { PORT, HOSTNAME, SESSION_SECRET} from './config/env.js';
 import routes from './routes/index.route.js';
 import prisma from './utils/client.js';
-import session from 'express-session';
+import session, { MemoryStore } from 'express-session';
 import cors from 'cors'
 import compression from 'compression';
 import responseTime from 'response-time';
 import morgan from 'morgan'
+import timeout from 'connect-timeout';
 
 const app = express()
+app.use(timeout('7s'));
 app.use(morgan('dev'));
 app.use(responseTime());
 app.use(express.json());
@@ -44,21 +46,4 @@ app.listen(PORT, HOSTNAME, () => {
 });
 
 // root for testing
-app.get('/', async (req, res) => {
-    if (!req.session.user) {
-        return res.status(400).json({
-            message: "Invalid Credentials"
-        })
-    }
 
-    const user = await prisma.tb_users.findUnique({
-        where: {
-            NIK: req.session.user.NIK
-        }
-    });
-
-    res.status(200).json({
-        message: `Welcome ${user.fullname} you login at ${req.session.user.loginDate}`,
-        data: user,
-    })
-})
