ff33a1d7bc91a36a2580ffcbb0df00f0f693c982
Andhika Dwitama <andhikaDWA@gmail.com>
2025-07-15 11:36:39 +0700

    temporary remove isAuth middlewares for integrates backend and fronted

--- server/src/routes/leave.route.js
+++ server/src/routes/leave.route.js
@@ -9,8 +9,11 @@ import { mandatoryLeaveForm, mandatoryLeaveFormUpdate } from "../validators/mand
 
 const leaveRoutes = express.Router();
 
-leaveRoutes.get('/logs', validateRole('super_admin', 'admin'), historyLeave);
-leaveRoutes.get('/logs/search', validateRole('super_admin', 'admin'), historyLeaveSearch);
+// temporaly
+leaveRoutes.get('/logs',  historyLeave);
+leaveRoutes.get('/logs/search', historyLeaveSearch);
+// leaveRoutes.get('/logs', validateRole('super_admin', 'admin'), historyLeave);
+// leaveRoutes.get('/logs/search', validateRole('super_admin', 'admin'), historyLeaveSearch);
 
 leaveRoutes.get('/special', getSpecialLeave)
 leaveRoutes.post('/special', validate(specialLeaveForm), createSpecialLeave)
