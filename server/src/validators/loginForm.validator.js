660dfb4a26dd47404cd6d6c00697f704d8a32a5d
Andhika Dwitama <andhikaDWA@gmail.com>
2025-07-17 20:44:10 +0700

    merge purpose

--- server/src/validators/loginForm.validator.js
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod/v4';
-
-const loginFormRequest = z.object({
-    email: z.email(),
-    password: z.string().min(8).max(16)
-});
-
-export default loginFormRequest;
--- server/src/validators/updateLeave.validator.js
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod/v4'
-
-const updateLeaveRequestSchema = z.object({
-    status: z.enum(['rejected', 'approved']),
-    reason: z.string().max(255)
-})
-
-export default updateLeaveRequestSchema;
